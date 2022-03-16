088f5c3bfd4f476eb0cc1e11dbca7a3fe750bda4
gauravsewani <lavishgps@gmail.com>
2022-03-16 13:22:29 +0530

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -101,7 +101,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="/"
+              href="https://www.linkedin.com/in/gaurav-sewani-69a736234"
             >
               Linkedin <FaLinkedinIn size={30} />
             </a>
@@ -117,7 +117,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#732441]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="/"
+              href="mailto:lavishgps@gmail.com"
             >
               Email <HiOutlineMail size={30} />
             </a>
