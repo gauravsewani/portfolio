28dc7c68a6f2fce0c9fc1cfefe56365e865fe773
gauravsewani <lavishgps@gmail.com>
2022-03-17 20:30:10 +0530

    updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,9 +15,9 @@ const Home = () => {
           I'm a Frontend Developer.
         </h2>
         <p className="text-[#8892b0] py-4 max-w-[700px]">
-          I’m a Frontend developer specializing in building (and occasionally
-          designing) exceptional digital experiences. Currently, I’m focused on
-          building responsive websites.
+          I’m a Self-taught Frontend developer specializing in building (and
+          occasionally designing) exceptional digital experiences. Currently,
+          I’m focused on building responsive websites.
         </p>
         <div>
           <Link to="work" smooth={true} duration={500}>
